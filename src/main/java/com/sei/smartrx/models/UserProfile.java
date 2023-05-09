2dab094072991537f7130640b3db38fa7ed78d33
builde7b0b <ggives3@gmail.com>
2023-05-09 13:16:18 -0700

    add implement and annotate

--- src/main/java/com/sei/smartrx/models/UserProfile.java
+++ src/main/java/com/sei/smartrx/models/UserProfile.java
@@ -1,6 +1,15 @@
 package com.sei.smartrx.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_profiles")
 public class UserProfile {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.AUTO)
+     private Long userProfile_id;
+    @Column
     private String role;
-    
+
 }
