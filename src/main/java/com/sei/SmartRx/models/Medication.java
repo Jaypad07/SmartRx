6d561cb32d99f784c48ac3aca1c2b66af9f28ffd
builde7b0b <ggives3@gmail.com>
2023-05-09 12:37:23 -0700

    gen constructors

--- src/main/java/com/sei/SmartRx/models/Medication.java
+++ src/main/java/com/sei/SmartRx/models/Medication.java
@@ -5,9 +5,22 @@ public class Medication {
     private Long id;
 
     private String name;
+
+    private String genericName;
     private String contraIndication;
     private String sideEffects;
     private String indredients;
 
 
+    public Medication() {
+    }
+
+    public Medication(Long id, String name, String genericName, String contraIndication, String sideEffects, String indredients) {
+        this.id = id;
+        this.name = name;
+        this.genericName = genericName;
+        this.contraIndication = contraIndication;
+        this.sideEffects = sideEffects;
+        this.indredients = indredients;
+    }
 }
