d02b64a38184bd393d2e2bdeae23e42fbaaba6b0
builde7b0b <ggives3@gmail.com>
2023-05-09 12:36:16 -0700

    add variables

---
+++ src/main/java/com/sei/SmartRx/models/Medication.java
@@ -0,0 +1,13 @@
+package com.sei.SmartRx.models;
+
+public class Medication {
+
+    private Long id;
+
+    private String name;
+    private String contraIndication;
+    private String sideEffects;
+    private String indredients;
+
+
+}
