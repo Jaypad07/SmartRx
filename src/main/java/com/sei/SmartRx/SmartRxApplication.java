3231a7bc12e23e8ba1077b6a1ea2c15919168823
Jaypad07 <jaypad07@gmail.com>
2023-05-09 09:46:26 -0700

    testing

---
+++ src/main/java/com/sei/SmartRx/SmartRxApplication.java
@@ -0,0 +1,13 @@
+package com.sei.SmartRx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartRxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sei/SmartRx/SmartRxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sei.SmartRx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
