a6ca2725640ffa89126e2166dc4c9edd10142294
vnpugh <vnp19@yahoo.com>
2023-05-09 14:19:02 -0500

    adding User Model

--- src/main/java/com/sei/SmartRx/models/Users.java
+++ src/main/java/com/sei/SmartRx/models/Users.java
@@ -1,4 +1,106 @@
 package com.sei.SmartRx.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "users")
 public class Users {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column
+    private LocalDate dob;
+
+    @Column
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @OneToOne(cascade = CascadeType.ALL)//pull the user and the profile as well
+    @JoinColumn(name = "profile_id", referencedColumnName = "id")
+    private UserProfile userProfile;
+
+    public Users() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserProfile getUserProfile() {
+        return userProfile;
+    }
+
+    public void setUserProfile(UserProfile userProfile) {
+        this.userProfile = userProfile;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", dob=" + dob +
+                ", password='" + password + '\'' +
+                ", userProfile=" + userProfile +
+                '}';
+    }
 }
