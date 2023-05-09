d5a01f646632bff62633901eb073283f8a0d4ef2
builde7b0b <ggives3@gmail.com>
2023-05-09 12:37:48 -0700

    gen getters, setters

--- src/main/java/com/sei/SmartRx/models/Medication.java
+++ src/main/java/com/sei/SmartRx/models/Medication.java
@@ -23,4 +23,52 @@ public class Medication {
         this.sideEffects = sideEffects;
         this.indredients = indredients;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenericName() {
+        return genericName;
+    }
+
+    public void setGenericName(String genericName) {
+        this.genericName = genericName;
+    }
+
+    public String getContraIndication() {
+        return contraIndication;
+    }
+
+    public void setContraIndication(String contraIndication) {
+        this.contraIndication = contraIndication;
+    }
+
+    public String getSideEffects() {
+        return sideEffects;
+    }
+
+    public void setSideEffects(String sideEffects) {
+        this.sideEffects = sideEffects;
+    }
+
+    public String getIndredients() {
+        return indredients;
+    }
+
+    public void setIndredients(String indredients) {
+        this.indredients = indredients;
+    }
 }
