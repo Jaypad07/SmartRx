b8e6f912bc7a8fe6e981288ee89962c6e6abf8ac
Kelsey Halley <kelseyghalley@gmail.com>
2023-05-09 11:59:32 -0700

    imported JPA dependency for annotations

--- src/main/java/com/sei/SmartRx/models/Prescription.java
+++ src/main/java/com/sei/SmartRx/models/Prescription.java
@@ -1,8 +1,14 @@
 package com.sei.SmartRx.models;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name="prescriptions")
 public class Prescription {
+    @Column
+    @Id
+
     Long prescriptionId;
     String patientName;
     String medicationName;
