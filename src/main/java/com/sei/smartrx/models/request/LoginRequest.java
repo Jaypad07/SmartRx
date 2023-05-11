6e68776ed9583e1f0b7cd1a1acef98a5e10a6534
Kelsey Halley <kelseyghalley@gmail.com>
2023-05-11 10:22:40 -0700

    filled in login req/response classes

--- src/main/java/com/sei/smartrx/models/request/LoginRequest.java
+++ src/main/java/com/sei/smartrx/models/request/LoginRequest.java
@@ -1,4 +1,15 @@
 package com.sei.smartrx.models.request;
 
 public class LoginRequest {
+    private String email;
+    private String password;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
