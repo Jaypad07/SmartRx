2917ce15f9564427fc0cfcb5f0a9689773a17dfb
vnpugh <vnp19@yahoo.com>
2023-05-11 12:29:35 -0500

    Created MyUserDetails & Service

---
+++ src/main/java/com/sei/smartrx/security/MyUserDetailsService.java
@@ -0,0 +1,44 @@
+package com.sei.smartrx.security;
+
+
+
+import com.sei.smartrx.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+public class MyUserDetailsService implements UserDetailsService {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(email);
+        return new MyUserDetails(user);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
