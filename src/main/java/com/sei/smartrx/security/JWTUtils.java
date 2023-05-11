15511a6676a533cf0bbb8aa7983b5d42893ee6e9
Kelsey Halley <kelseyghalley@gmail.com>
2023-05-11 10:28:47 -0700

    added JWTUtils class

---
+++ src/main/java/com/sei/smartrx/security/JWTUtils.java
@@ -0,0 +1,55 @@
+package com.sei.smartrx.security;
+
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Service
+public class JWTUtils {
+
+    Logger logger = Logger.getLogger(JWTUtils.class.getName());
+    //inserts our variables during runtime, not hardcoded into the app
+    @Value("${jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${jwt-expiration-ms}")
+    private int jwtExpirationMs;
+
+    public String generateJwtToken(MyUserDetails myUserDetails) {
+        //inject userdetails, to get username. Sets the issue date. Then we set the expiration date. Create a secure signature with server security key that gets loaded
+        //during runtime. Compact means build token.
+        return Jwts.builder()
+                .setSubject((myUserDetails.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
+    //gets the subject/payload for token
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SecurityException e) {
+            logger.log(Level.SEVERE, "Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            logger.log(Level.SEVERE, "Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.log(Level.SEVERE, "JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.log(Level.SEVERE, "JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.log(Level.SEVERE, "JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+}
