f8829b87664b11c90f2c5f8fab3f374da72ad31c
Jaypad07 <jaypad07@gmail.com>
2023-05-11 09:01:33 -0700

    Test: Get a medication feature passed all tests.

--- src/main/java/com/sei/smartrx/seed/PrescriptionDataLoader.java
+++ src/main/java/com/sei/smartrx/seed/PrescriptionDataLoader.java
@@ -1,7 +1,9 @@
 package com.sei.smartrx.seed;
 
+import com.sei.smartrx.models.Medication;
 import com.sei.smartrx.models.Prescription;
 import com.sei.smartrx.models.User;
+import com.sei.smartrx.repository.MedicationRepository;
 import com.sei.smartrx.repository.PrescriptionRepository;
 import com.sei.smartrx.repository.UserRepository;
 import org.slf4j.LoggerFactory;
@@ -24,6 +26,9 @@ public class PrescriptionDataLoader implements CommandLineRunner {
     PrescriptionRepository prescriptionRepository;
 
     @Autowired
+    MedicationRepository medicationRepository;
+
+    @Autowired
     UserRepository userRepository;
 
     @Override
@@ -41,6 +46,8 @@ public class PrescriptionDataLoader implements CommandLineRunner {
             Prescription prescription3 = new Prescription("Eric Slack", 2, currentDate, false);
 
             User user = new User("Stacey", "Smith", "email@email.com", currentDate, "password", "watermelon");
+            Medication medication1 = new Medication(1L, "name", "genName", "contra", "sideEffects", "ingred");
+            medicationRepository.save(medication1);
 
             // user can have many prescriptions
             List<Prescription> prescriptionList = new ArrayList<>();
