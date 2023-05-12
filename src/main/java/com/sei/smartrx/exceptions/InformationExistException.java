b8007d0f960f7f6f8b527750ccdfece7da883832
Kelsey Halley <kelseyghalley@gmail.com>
2023-05-11 21:25:21 -0700

    added doc strings to excetions classes

--- src/main/java/com/sei/smartrx/exceptions/InformationExistException.java
+++ src/main/java/com/sei/smartrx/exceptions/InformationExistException.java
@@ -3,6 +3,9 @@ package com.sei.smartrx.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * useful to have custom error messages to be more informative for developer and/or user
+ */
 @ResponseStatus(HttpStatus.CONFLICT)
 public class InformationExistException extends RuntimeException{
     public InformationExistException(String message){
--- src/main/java/com/sei/smartrx/exceptions/InformationNotFoundException.java
+++ src/main/java/com/sei/smartrx/exceptions/InformationNotFoundException.java
@@ -2,7 +2,9 @@ package com.sei.smartrx.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+/**
+ * useful to have custom error messages to be more informative for developer and/or user
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class InformationNotFoundException extends RuntimeException{
     public InformationNotFoundException(String message){
