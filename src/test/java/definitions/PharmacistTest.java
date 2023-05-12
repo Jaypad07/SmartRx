5aa14e49add565a04c8c9d8fb67433c4b65f0aa4
builde7b0b <ggives3@gmail.com>
2023-05-12 13:02:20 -0700

    create step definitions

--- src/test/java/definitions/PharmacistTest.java
+++ src/test/java/definitions/PharmacistTest.java
@@ -1,9 +1,20 @@
 package definitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class PharmacistTest {
     @Given("a User had authorization role of pharmacist")
     public void aUserHadAuthorizationRoleOfPharmacist() {
+    }
+
+    @When("the pharmacist deletes a prescription")
+    public void thePharmacistDeletesAPrescription() {
+        
+    }
+
+    @Then("the prescription is deleted")
+    public void thePrescriptionIsDeleted() {
     }
 }
