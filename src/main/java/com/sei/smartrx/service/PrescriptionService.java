3868dda13847ee5c23b9f843747b6bb3b5d758c0
wealthybeans <wealthybeans@wealthybeanss-MacBook-Air.local>
2023-05-13 12:45:57 -0700

    add docstrings

--- src/main/java/com/sei/smartrx/service/PrescriptionService.java
+++ src/main/java/com/sei/smartrx/service/PrescriptionService.java
@@ -21,21 +21,46 @@ public class PrescriptionService {
     @Autowired
     private MedicationRepository medicationRepository;
 
+
+    /**
+     * Sets the MedicationRepository dependency.
+     *
+     * @param medicationRepository the MedicationRepository
+     */
     @Autowired
     public void setMedicationRepository(MedicationRepository medicationRepository) {
         this.medicationRepository = medicationRepository;
     }
+
+    /**
+     * sets the PrescriptionRepository dependency.
+     *
+     * @param prescriptionRepository the PrescriptionRepository
+     */
 
     @Autowired
     public void setPrescriptionRepository(PrescriptionRepository prescriptionRepository) {
         this.prescriptionRepository = prescriptionRepository;
     }
     //---------------------------------------------
+
+    /**
+     * Retrives the current logged-in user
+     *
+     * @return the User instance representing the current logged-in user
+     */
     public static User getCurrentLoggedInUser() {
         MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return userDetails.getUser();
     }
     //---------------------------------------------
+
+    /**
+     * Retrives all prescriptions
+     *
+     * @return a list of all prescriptions
+     * @throws InformationNotFoundException if no previous prescriptions are found
+     */
 
     public List<Prescription> getAllPrescriptions() {
         List<Prescription> prescriptionList = prescriptionRepository.findAll();
@@ -44,12 +69,27 @@ public class PrescriptionService {
         }else return prescriptionList;
     }
 
+    /**
+     * Retrieves all prescriptions for the current logged-in user
+     *
+     * @return a list of prescriptions for the current user
+     * @throws InformationNotFoundException if no previous prescriptions are found for the current user
+     */
+
     public List<Prescription> getAllPrescriptionsForUser() {
         List<Prescription> prescriptionList = prescriptionRepository.findByUserId(getCurrentLoggedInUser().getId()).get();
         if (prescriptionList.size() == 0) {
             throw new InformationNotFoundException("No previous prescriptions found for user with id of " + getCurrentLoggedInUser().getId());
         } else return prescriptionList;
     }
+
+    /**
+     * Retrieves a medication by its ID.
+     *
+     * @param medicationId the ID of the medication to retrieve
+     * @return the Medication instance corresponding to the provided ID
+     * @throws InformationNotFoundException if now medication is found with the provided ID
+     */
 
     public Medication seeAMedication(Long medicationId){
         Optional<Medication> medication = medicationRepository.findById(medicationId);
@@ -61,6 +101,13 @@ public class PrescriptionService {
         }
     }
 
+    /**
+     * Requests a prescription refill by its ID
+     * @param prescriptionId the ID of the prescription to refill
+     * @return the Prescription instance corresponding to the ID provided
+     * @throws InformationNotFoundException if no prescription is found with the provided ID
+     */
+
     public Prescription requestPrescriptionRefill(Long prescriptionId){
         Optional<Prescription> refillPrescription = Optional.of(prescriptionRepository.getById(prescriptionId));
         if(refillPrescription.isEmpty()){
@@ -70,7 +117,7 @@ public class PrescriptionService {
             return refillPrescription.get();
         }
     }
-//testing purposes
+
 
 
 }
